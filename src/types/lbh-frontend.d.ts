89f124522a400a13a480e905f709ae486c623d58
Gareth Cozens <gareth.cozens@amido.com>
2021-06-01 09:11:26 +0100

    fix: add missing typings

---
+++ src/types/lbh-frontend.d.ts
@@ -0,0 +1,22 @@
+/* eslint-disable max-classes-per-file */
+declare module 'lbh-frontend' {
+  export class Accordion {
+    constructor(module: HTMLDivElement);
+
+    init(): void;
+
+    setExpanded(expanded: boolean, section: HTMLDivElement | null): void;
+
+    isExpanded(section: HTMLDivElement | null): boolean;
+
+    $sections: NodeListOf<HTMLDivElement>;
+
+    sectionButtonClass: string;
+  }
+
+  export class ErrorSummary {
+    constructor(module: HTMLDivElement);
+
+    init(): void;
+  }
+}
