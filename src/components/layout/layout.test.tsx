53843adb1a9c619f39ce37a2466fa9c64d2cc8e4
Gareth Cozens <gareth.cozens@amido.com>
2021-06-04 12:48:19 +0100

    feat(components): add Layout and SideBar components

---
+++ src/components/layout/layout.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { testA11y } from '../../test-utils';
+import { Layout } from './layout';
+
+test('it renders correctly', async () => {
+  const { container } = render(
+    <Layout
+      backLabel="Back"
+      backLink="http://localhost"
+      side={<div id="side">Side Bar</div>}
+      top={<div id="top">Top</div>}
+    >
+      <div id="main">Main</div>
+    </Layout>
+  );
+  expect(container).toMatchSnapshot();
+  await testA11y(container);
+});
+
+test('it renders correctly without a side', () => {
+  const { container } = render(
+    <Layout backLabel="Back" backLink="http://localhost">
+      <div id="main">Main</div>
+    </Layout>
+  );
+  expect(container).toMatchSnapshot();
+});
