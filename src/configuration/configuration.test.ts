9918b5f55fe0b88be1e5b5830dbc033218b64005
RVerma90 <irahulverma90@gmail.com>
2021-08-17 14:43:11 +0300

    Removed SearchTenure, added SearchAsset toggles (#31)
    Co-authored-by: Rahul Verma <rahulverma@AML0341.lan>

--- src/configuration/configuration.test.ts
+++ src/configuration/configuration.test.ts
@@ -13,42 +13,42 @@ beforeEach(() => {
 });
 
 test('configuration is set from instatiation', () => {
-  expect(hasToggle('MMH.SearchTenure')).toBe(false);
+  expect(hasToggle('MMH.SearchAsset')).toBe(false);
 });
 
 test('configuration is hydrated from localStorage first', async () => {
   getSuccess([], 'api/v1/configuration');
   window.localStorage.setItem(
     'features',
-    JSON.stringify({ MMH: { SearchTenure: true } })
+    JSON.stringify({ MMH: { SearchAsset: true } })
   );
   await getConfiguration();
-  expect(hasToggle('MMH.SearchTenure')).toBe(true);
+  expect(hasToggle('MMH.SearchAsset')).toBe(true);
 });
 
 test('configuration not hydrated if localStorage is malformed', async () => {
   getSuccess([], 'api/v1/configuration');
   window.localStorage.setItem('features', JSON.stringify(1));
   await getConfiguration();
-  expect(hasToggle('MMH.SearchTenure')).toBe(false);
+  expect(hasToggle('MMH.SearchAsset')).toBe(false);
 });
 
 test('configuration is hydrated from api', async () => {
   getSuccess(
-    [{ type: 'MMH', featureToggles: { SearchTenure: true } }],
+    [{ type: 'MMH', featureToggles: { SearchAsset: true } }],
     'api/v1/configuration'
   );
   await getConfiguration();
-  expect(hasToggle('MMH.SearchTenure')).toBe(true);
+  expect(hasToggle('MMH.SearchAsset')).toBe(true);
 });
 
 test('configuration is persisted to localStorage on success', async () => {
   getSuccess(
-    [{ type: 'MMH', featureToggles: { SearchTenure: true } }],
+    [{ type: 'MMH', featureToggles: { SearchAsset: true } }],
     'api/v1/configuration'
   );
   await getConfiguration();
   expect(window.localStorage.getItem('features')).toBe(
-    '{"MMH":{"SearchTenure":true}}'
+    '{"MMH":{"SearchAsset":true}}'
   );
 });
