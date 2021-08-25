852e90be6ef5d14d2026cc9e8e471afde2d6f46d
Gareth Cozens <gareth.cozens@amido.com>
2021-08-25 14:04:22 +0100

    feat(feature-toggle): add useFeatureToggle hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
 export * from './use-axios-swr';
 export * from './use-breakpoint';
+export * from './use-feature-toggle';
---
+++ src/hooks/tests/use-feature-toggle.test.ts
@@ -0,0 +1,18 @@
+import { act, renderHook } from '@testing-library/react-hooks';
+import { featureToggleStore } from '../../configuration';
+import { useFeatureToggle } from '../use-feature-toggle';
+
+describe('useFeatureToggle', () => {
+  test('it retrieves the correct toogle', () => {
+    const features = featureToggleStore.getValue();
+    const { result } = renderHook(() => useFeatureToggle('MMH.Test'));
+    expect(result.current).toBe(false);
+    act(() => {
+      featureToggleStore.next({ MMH: { ...features.MMH, Test: true } });
+    });
+    expect(result.current).toBe(true);
+    act(() => {
+      featureToggleStore.next(features);
+    });
+  });
+});
