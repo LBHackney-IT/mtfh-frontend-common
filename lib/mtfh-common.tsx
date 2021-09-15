25d76406c43fb57c486e1649772176779257628b
Gareth Cozens <gareth.cozens@amido.com>
2021-09-15 10:43:14 +0100

    fix(configuration): create entry point for configuration

--- lib/mtfh-common.tsx
+++ lib/mtfh-common.tsx
@@ -1,5 +1,5 @@
 export * from '@mtfh/common/lib/http';
 export * from '@mtfh/common/lib/auth';
+export * from '@mtfh/common/lib/configuration';
 export * from '@mtfh/common/lib/components';
 export * from '@mtfh/common/lib/hooks';
-export * from './configuration';
