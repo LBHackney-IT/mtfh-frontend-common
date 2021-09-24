6e1aafb4cc06e7a1e3ae1d06b559ef31c0a67b3f
Rahul Verma <rahulverma@AML0341.local>
2021-09-24 20:28:22 +0300

    feat(useErrorCodes): implemented cases for when call to errorcode refData resolves with data and error

--- lib/hooks/tests/use-error-codes.test.ts
+++ lib/hooks/tests/use-error-codes.test.ts
@@ -1,11 +1,42 @@
 import { renderHook } from '@testing-library/react-hooks';
+import { useReferenceData } from '../../api/reference-data/v1';
 import locale from '../../locale';
 import { useErrorCodes } from '../use-error-codes';
 
+
+jest.mock('../../api/reference-data/v1');
 const { hooks } = locale;
 
 describe('useErrorCodes', () => {
-  test('it returns the default error codes and messages', () => {
+  test('it makes a call to the referenceData API for error codes and returns them', () => {
+    const $useReferenceDataMock = useReferenceData as jest.Mock;
+    $useReferenceDataMock.mockReturnValueOnce({
+      data: hooks.errorCodes,
+      error: null,
+    });
+
+    const { result } = renderHook(() => useErrorCodes());
+    expect(result.current).toBe(hooks.errorCodes);
+  });
+
+  test('it returns null if call to the referenceData API has not resolved', () => {
+    const $useReferenceDataMock = useReferenceData as jest.Mock;
+    $useReferenceDataMock.mockReturnValueOnce({
+      data: undefined,
+      error: undefined,
+    });
+
+    const { result } = renderHook(() => useErrorCodes());
+    expect(result.current).toBe(null);
+  });
+
+  test('it returns default hooks error codes if call to the referenceData API has failed', () => {
+    const $useReferenceDataMock = useReferenceData as jest.Mock;
+    $useReferenceDataMock.mockReturnValueOnce({
+      data: undefined,
+      error: { status: 404, message: 'Cannot retrieve error code' },
+    });
+
     const { result } = renderHook(() => useErrorCodes());
     expect(result.current).toBe(hooks.errorCodes);
   });
--- lib/hooks/use-error-codes.ts
+++ lib/hooks/use-error-codes.ts
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useReferenceData } from '../api/reference-data/v1';
 import locale from '../locale';
 
 export const useErrorCodes = () => {
-  const [errors] = useState(locale.hooks.errorCodes);
+  const [errors, setErrors] = useState<any>(locale.hooks.errorCodes);
+
+  const { data, error } = useReferenceData(
+    { category: 'error-code', subCategory: 'mmh' },
+    {
+      revalidateIfStale: false,
+    }
+  );
+
+  if (!data && !error) {
+    return null;
+  }
+
+  if (error) {
+    return errors;
+  }
+
+  useEffect(() => {
+    setErrors(data);
+  }, [data]);
 
   return errors;
 };
