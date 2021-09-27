a7af23fce094fdb4eb69708fbc58982fd78c9b4d
Rahul Verma <rahulverma@aml0341.home>
2021-09-27 17:14:25 +0100

    feat(error-codes): return errorMessages from referenceData, with fallback messages for Wx code not returned from api

--- lib/locale.ts
+++ lib/locale.ts
@@ -31,137 +31,51 @@ const locale = {
     },
   },
   hooks: {
-    errorCodes: [
-      {
-        W1: 'You must correct the indicated errors',
-      },
-      {
-        W2: 'You must enter a description for this comment',
-      },
-      {
-        W3: 'You have 500 characters remaining',
-      },
-      {
-        W4: 'You must select an option to proceed',
-      },
-      {
-        W5: 'You must select a title to proceed',
-      },
-      {
-        W6: 'You must enter this information to proceeed',
-      },
-      {
-        W7: 'Date of birth must be in numerical format',
-      },
-      {
-        W8: 'You must remove special characters to proceed e.g. #@%$',
-      },
-      {
-        W9: 'You must enter a valid date to proceed',
-      },
-      {
-        W10: 'This date cannot be in the future',
-      },
-      {
-        W11: 'You must enter a valid month to proceed e.g. 01-12',
-      },
-      {
-        W12: 'You must enter a valid day to proceed e.g. 01-31',
-      },
-      {
-        W13: 'You must enter a valid year to proceed e.g. 1980',
-      },
-      {
-        W14: 'You must select a person type to proceed',
-      },
-      {
-        W15: 'You must enter a first name for this person',
-      },
-      {
-        W16: 'You must enter a last name for this person',
-      },
-      {
-        W17: 'You must enter a reason for creating this person',
-      },
-      {
-        W18: 'You must select a language or remove the language spoken',
-      },
-      {
-        W19: 'You must enter the ID number or remove the ID document',
-      },
-      {
-        W20: 'You must indicate if you have seen the ID document',
-      },
-      {
-        W21: 'You must select a gender',
-      },
-      {
-        W22: 'You must select a nationality',
-      },
-      {
-        W23: 'You must enter a valid National Insurance number',
-      },
-      {
-        W24: 'You must indicate one language as primary',
-      },
-      {
-        W25: 'You cannot enter more than 10 languages',
-      },
-      {
-        W26: 'You cannot enter more than 5 IDs',
-      },
-      {
-        W27: 'You must enter at least 2 characters.',
-      },
-      {
-        W29: 'End date must occur after start date',
-      },
-      {
-        W30: 'Start date must occur after the end date of the previous tenure',
-      },
-      {
-        W31: 'You must provide a title for this comment',
-      },
-      {
-        W32: 'You must select a category for this comment',
-      },
-      {
-        W33: 'You must select a valid type to proceed',
-      },
-      {
-        W35: 'You must enter a phone number to proceed',
-      },
-      {
-        W36: 'You must select a valid type to proceed',
-      },
-      {
-        W38: 'You must enter an email address to proceed',
-      },
-      {
-        W40: 'You must enter a valid phone number to proceed',
-      },
-      {
-        W41: 'You must enter a valid phone number to proceed',
-      },
-      {
-        W43: 'You cannot add more than xx contact details',
-      },
-      {
-        W44: 'We encountered an error. Try again.',
-      },
-      {
-        W45: 'You cannot add more than 4 tenure holders',
-      },
-      {
-        W46: 'Person could not be created',
-      },
-      {
-        W47: 'Person created but could not be added to tenure',
-      },
-      {
-        W48: 'Person created but could not be added to tenure',
-      },
-    ],
+    errorMessages: {
+      W1: 'You must correct the indicated errors',
+      W2: 'You must enter a description for this comment',
+      W3: 'You have 500 characters remaining',
+      W4: 'You must select an option to proceed',
+      W5: 'You must select a title to proceed',
+      W6: 'You must enter this information to proceeed',
+      W7: 'Date of birth must be in numerical format',
+      W8: 'You must remove special characters to proceed e.g. #@%$',
+      W9: 'You must enter a valid date to proceed',
+      W10: 'This date cannot be in the future',
+      W11: 'You must enter a valid month to proceed e.g. 01-12',
+      W12: 'You must enter a valid day to proceed e.g. 01-31',
+      W13: 'You must enter a valid year to proceed e.g. 1980',
+      W14: 'You must select a person type to proceed',
+      W15: 'You must enter a first name for this person',
+      W16: 'You must enter a last name for this person',
+      W17: 'You must enter a reason for creating this person',
+      W18: 'You must select a language or remove the language spoken',
+      W19: 'You must enter the ID number or remove the ID document',
+      W20: 'You must indicate if you have seen the ID document',
+      W21: 'You must select a gender',
+      W22: 'You must select a nationality',
+      W23: 'You must enter a valid National Insurance number',
+      W24: 'You must indicate one language as primary',
+      W25: 'You cannot enter more than 10 languages',
+      W26: 'You cannot enter more than 5 IDs',
+      W27: 'You must enter at least 2 characters.',
+      W29: 'End date must occur after start date',
+      W30: 'Start date must occur after the end date of the previous tenure',
+      W31: 'You must provide a title for this comment',
+      W32: 'You must select a category for this comment',
+      W33: 'You must select a valid type to proceed',
+      W35: 'You must enter a phone number to proceed',
+      W36: 'You must select a valid type to proceed',
+      W38: 'You must enter an email address to proceed',
+      W40: 'You must enter a valid phone number to proceed',
+      W41: 'You must enter a valid phone number to proceed',
+      W43: 'You cannot add more than xx contact details',
+      W44: 'We encountered an error. Try again.',
+      W45: 'You cannot add more than 4 tenure holders',
+      W46: 'Person could not be created',
+      W47: 'Person created but could not be added to tenure',
+      W48: 'Person created but could not be added to tenure',
+    },
   },
 };
 
