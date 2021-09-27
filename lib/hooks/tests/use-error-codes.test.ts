11d07056d9ef833ccf191552a32812c23819a022
Rahul Verma <rahulverma@aml0341.home>
2021-09-27 17:14:25 +0100

    feat(error-codes): return errorMessages from referenceData, with fallback messages for Wx code not returned from api

--- lib/hooks/tests/use-error-codes.test.ts
+++ lib/hooks/tests/use-error-codes.test.ts
@@ -3,24 +3,23 @@ import { useReferenceData } from '../../api/reference-data/v1';
 import locale from '../../locale';
 import { useErrorCodes } from '../use-error-codes';
 
-
 jest.mock('../../api/reference-data/v1');
 const { hooks } = locale;
+const { errorMessages } = hooks;
 
 describe('useErrorCodes', () => {
-  test('it makes a call to the referenceData API for error codes and returns them', () => {
-    const $useReferenceDataMock = useReferenceData as jest.Mock;
+  const $useReferenceDataMock = useReferenceData as jest.Mock;
+  test('it returns default error codes them', () => {
     $useReferenceDataMock.mockReturnValueOnce({
-      data: hooks.errorCodes,
+      data: [{}],
       error: null,
     });
 
     const { result } = renderHook(() => useErrorCodes());
-    expect(result.current).toBe(hooks.errorCodes);
+    expect(result.current).toBe(errorMessages);
   });
 
   test('it returns null if call to the referenceData API has not resolved', () => {
-    const $useReferenceDataMock = useReferenceData as jest.Mock;
     $useReferenceDataMock.mockReturnValueOnce({
       data: undefined,
       error: undefined,
@@ -30,14 +29,51 @@ describe('useErrorCodes', () => {
     expect(result.current).toBe(null);
   });
 
-  test('it returns default hooks error codes if call to the referenceData API has failed', () => {
-    const $useReferenceDataMock = useReferenceData as jest.Mock;
+  test('it returns default errorMessages if call to the referenceData API has failed', () => {
     $useReferenceDataMock.mockReturnValueOnce({
       data: undefined,
       error: { status: 404, message: 'Cannot retrieve error code' },
     });
 
     const { result } = renderHook(() => useErrorCodes());
-    expect(result.current).toBe(hooks.errorCodes);
+    expect(result.current).toBe(errorMessages);
+  });
+
+  test('it makes a call to the referenceData API for error codes and returns them', () => {
+    const mergedErrorMessages = {
+      ...errorMessages,
+      W50: 'W50TEXT_TEST',
+      W51: 'W51TEXT_TEST',
+    };
+    $useReferenceDataMock.mockReturnValue({
+      data: [
+        {
+          id: '93344749-2602-451b-5d17-4c3764330203',
+          category: 'error-code',
+          subCategory: 'mmh',
+          code: 'W50',
+          value: 'W50TEXT_TEST',
+          description: 'Contact type',
+          isActive: true,
+          createdAt: '2021-09-24T11:13:19.13661',
+          tags: ['Housing'],
+        },
+        {
+          id: '7c1954f3-bd74-0d86-fbf5-d72e0c1a912d',
+          category: 'error-code',
+          subCategory: 'mmh',
+          code: 'W51',
+          value: 'W51TEXT_TEST',
+          description: 'Selection mandatory',
+          isActive: true,
+          createdAt: '2021-09-24T11:13:16.561198',
+          tags: ['Housing'],
+        },
+      ],
+      error: null,
+    });
+
+    const { result } = renderHook(() => useErrorCodes());
+    expect(result.current).toStrictEqual(mergedErrorMessages);
   });
 });
--- lib/hooks/use-error-codes.ts
+++ lib/hooks/use-error-codes.ts
@@ -3,7 +3,7 @@ import { useReferenceData } from '../api/reference-data/v1';
 import locale from '../locale';
 
 export const useErrorCodes = () => {
-  const [errors, setErrors] = useState<any>(locale.hooks.errorCodes);
+  const [errors, setErrors] = useState<any>(locale.hooks.errorMessages);
 
   const { data, error } = useReferenceData(
     { category: 'error-code', subCategory: 'mmh' },
@@ -21,7 +21,23 @@ export const useErrorCodes = () => {
   }
 
   useEffect(() => {
-    setErrors(data);
+    const fromErrorsDefaultData = Object.entries(errors).map((e, i) => ({
+      [e[0]]: e[1],
+    }));
+
+    const fromErrorsReferenceData =
+      data.map((item) => ({
+        [item.code]: item.value,
+      })) || [];
+
+    const mergedErrors = [
+      ...new Set([...fromErrorsDefaultData, ...fromErrorsReferenceData]),
+    ].reduce((obj, item) => {
+      const errorCode = Object.keys(item)[0];
+      obj[errorCode] = item[errorCode];
+      return obj;
+    }, {});
+    setErrors(mergedErrors);
   }, [data]);
 
   return errors;
