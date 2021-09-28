81c023cee05d0d47bf1b194154a036b54fe6b37a
Rahul Verma <rahulverma@aml0341.home>
2021-09-28 13:14:11 +0100

    feat(reference-data): refactored code following PR feedback

--- lib/hooks/use-error-codes.ts
+++ lib/hooks/use-error-codes.ts
@@ -2,8 +2,14 @@ import { useEffect, useState } from 'react';
 import { useReferenceData } from '../api/reference-data/v1';
 import locale from '../locale';
 
+interface ErrorMessages {
+  [key: string]: string;
+}
+
 export const useErrorCodes = () => {
-  const [errors, setErrors] = useState<any>(locale.hooks.errorMessages);
+  const [errorMessages, setErrorMessages] = useState<ErrorMessages>(
+    locale.hooks.errorMessages
+  );
 
   const { data, error } = useReferenceData(
     { category: 'error-code', subCategory: 'mmh' },
@@ -12,33 +18,38 @@ export const useErrorCodes = () => {
     }
   );
 
+  useEffect(() => {
+    if (data) {
+      const fromErrorsDefaultData = Object.entries(errorMessages).map(
+        ([key, value]) => ({
+          [key]: value,
+        })
+      );
+
+      const fromErrorsReferenceData =
+        // @ts-ignore
+        data.map((item: any) => ({
+          [item.code]: item.value,
+        })) || [];
+
+      const mergedErrors = [
+        ...new Set([...fromErrorsDefaultData, ...fromErrorsReferenceData]),
+      ].reduce((obj, item) => {
+        const errorCode = Object.keys(item)[0];
+        obj[errorCode] = item[errorCode];
+        return obj;
+      }, {});
+      setErrorMessages(mergedErrors);
+    }
+  }, [data]);
+
   if (!data && !error) {
     return null;
   }
 
   if (error) {
-    return errors;
+    return errorMessages;
   }
 
-  useEffect(() => {
-    const fromErrorsDefaultData = Object.entries(errors).map((e, i) => ({
-      [e[0]]: e[1],
-    }));
-
-    const fromErrorsReferenceData =
-      data.map((item) => ({
-        [item.code]: item.value,
-      })) || [];
-
-    const mergedErrors = [
-      ...new Set([...fromErrorsDefaultData, ...fromErrorsReferenceData]),
-    ].reduce((obj, item) => {
-      const errorCode = Object.keys(item)[0];
-      obj[errorCode] = item[errorCode];
-      return obj;
-    }, {});
-    setErrors(mergedErrors);
-  }, [data]);
-
-  return errors;
+  return errorMessages;
 };
