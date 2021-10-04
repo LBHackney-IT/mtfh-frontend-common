f0764cbe1ff3ce488049a22566d0a0ae401037f2
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-04 17:42:17 +0200

    test(tenure service): more coverage

--- lib/api/tenure/v1/service.test.ts
+++ lib/api/tenure/v1/service.test.ts
@@ -1,6 +1,11 @@
 import { AddTenureParams } from ".";
 import { config } from "@mtfh/common/lib/config";
-import { AxiosSWRConfiguration, axiosInstance, useAxiosSWR } from "@mtfh/common/lib/http";
+import {
+  AxiosSWRConfiguration,
+  axiosInstance,
+  mutate,
+  useAxiosSWR,
+} from "@mtfh/common/lib/http";
 import {
   AddPersonToTenureParams,
   EditTenureParams,
@@ -17,6 +22,7 @@ jest.mock("@mtfh/common/lib/http", () => ({
   ...jest.requireActual("@mtfh/common/lib/http"),
   axiosInstance: { patch: jest.fn(), post: jest.fn(), delete: jest.fn() },
   useAxiosSWR: jest.fn(),
+  mutate: jest.fn(),
 }));
 
 test("addPersonToTenure: it should send the right thing to the API", async () => {
@@ -43,10 +49,14 @@ test("addPersonToTenure: it should send the right thing to the API", async () =>
 });
 
 test("useTenure: it should send the right body to the API", async () => {
+  const returnedValue = { tenureId: "" };
   const id = "id";
   const options: AxiosSWRConfiguration<Tenure> = { dedupingInterval: 10 };
-  useTenure(id, options);
+  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(returnedValue);
+
+  const response = await useTenure(id, options);
   expect(useAxiosSWR).toBeCalledWith(`${config.tenureApiUrlV1}/tenures/${id}`, options);
+  expect(response).toBe(returnedValue);
 });
 
 test("addTenure: it should send the right body to the API", async () => {
@@ -66,9 +76,15 @@ test("addTenure: it should send the right body to the API", async () => {
   const tenureReturned = { id: "tenureId" };
   (axiosInstance.post as jest.Mock).mockResolvedValueOnce({ data: tenureReturned });
 
-  addTenure(params);
+  const response = await addTenure(params);
 
   expect(axiosInstance.post).toBeCalledWith(`${config.tenureApiUrlV1}/tenures`, params);
+  expect(mutate).toBeCalledWith(
+    `${config.tenureApiUrlV1}/tenures/${tenureReturned.id}`,
+    tenureReturned,
+    false,
+  );
+  expect(response).toBe(tenureReturned);
 });
 
 test("removePersonFromTenure: it should send the right body to the API", async () => {
