44e2b395ffb30b63d1cf9c48696d7b132b2a1cdf
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-18 16:35:41 +0200

    test(*): covers contact details v2

--- lib/api/contact-details/v2/service.ts
+++ lib/api/contact-details/v2/service.ts
@@ -20,29 +20,25 @@ export interface ContactDetailsResponse {
 export const useContactDetails = (
   id: string,
   options?: AxiosSWRConfiguration<ContactDetailsResponse>,
-): AxiosSWRResponse<ContactDetailsResponse> => {
-  return useAxiosSWR<ContactDetailsResponse>(
-    `${config.contactDetailsApiUrlV2}/contactDetails?targetId=${id}`,
-    {
-      ...options,
-      onErrorRetry: /* istanbul ignore next: unreachable in test suite */ (
-        error,
-        key,
-        config,
-        revalidate,
-        { retryCount },
-      ) => {
-        if (error.response?.status === 404) return;
-        if (retryCount >= 3) return;
+): AxiosSWRResponse<ContactDetailsResponse> =>
+  useAxiosSWR(id && `${config.contactDetailsApiUrlV2}/contactDetails?targetId=${id}`, {
+    ...options,
+    onErrorRetry: /* istanbul ignore next: unreachable in test suite */ (
+      error,
+      key,
+      config,
+      revalidate,
+      { retryCount },
+    ) => {
+      if (error.response?.status === 404) return;
+      if (retryCount >= 3) return;
 
-        const count = Math.min(retryCount, 8);
-        const timeout =
-          ~~((Math.random() + 0.5) * (1 << count)) * config.errorRetryInterval;
-        setTimeout(() => revalidate({ retryCount }), timeout);
-      },
+      const count = Math.min(retryCount, 8);
+      const timeout =
+        ~~((Math.random() + 0.5) * (1 << count)) * config.errorRetryInterval;
+      setTimeout(() => revalidate({ retryCount }), timeout);
     },
-  );
-};
+  });
 
 export const addContactDetail = async (
   id: string,
@@ -71,28 +67,22 @@ export const addEmailContact = async (
   id: string,
   email: string,
   description?: string,
-): Promise<ContactDetail> => {
-  const response = await addContactDetail(id, {
+): Promise<ContactDetail> =>
+  addContactDetail(id, {
     contactType: ContactInformationContactTypes.EMAIL,
     value: email,
     description,
   });
-
-  return response;
-};
 
 export const addPhoneContact = async (
   id: string,
   phone: string,
   type: ContactDetailsPhoneTypes,
   description?: string,
-): Promise<ContactDetail> => {
-  const response = await addContactDetail(id, {
+): Promise<ContactDetail> =>
+  addContactDetail(id, {
     contactType: ContactInformationContactTypes.PHONE,
     value: phone,
     subType: type,
     description,
   });
-
-  return response;
-};
