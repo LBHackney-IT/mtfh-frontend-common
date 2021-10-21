0df6b1b8e50fe2ac89f65a1101f54282e85b794d
srcrustaceo <ureciomunicio@deloitte.co.uk>
2021-10-21 14:25:51 +0200

    update(address api): improves response and erroring

--- lib/api/address/v1/service.ts
+++ lib/api/address/v1/service.ts
@@ -2,19 +2,26 @@ import { config } from "@mtfh/common/lib/config";
 import { axiosInstance } from "@mtfh/common/lib/http";
 import { Address } from "./types";
 
-interface SearchAddressResponse {
+interface AddressAPIResponse {
   data: { address: Address[] };
 }
 
-export const searchAddress = (postCode: string): Promise<Address[]> =>
+interface SearchAddressRespone {
+  addresses?: Address[];
+  error?: { code: number };
+}
+
+export const searchAddress = (postCode: string): Promise<SearchAddressRespone> =>
   axiosInstance
-    .get<SearchAddressResponse>(
-      `${config.addressApiUrlV1}/addresses?postcode=${postCode}`,
-      {
-        headers: {
-          "skip-x-correlation-id": true,
-        },
+    .get<AddressAPIResponse>(`${config.addressApiUrlV1}/addresses?postcode=${postCode}`, {
+      headers: {
+        "skip-x-correlation-id": true,
       },
-    )
-    .then((res) => res.data.data.address)
-    .catch((res) => res);
+    })
+    .then((res) => ({ addresses: res.data.data.address }))
+    .catch((res) => {
+      if (res.message.toLowerCase().indexOf("network") !== -1) {
+        return { error: { code: 500 } };
+      }
+      return res;
+    });
