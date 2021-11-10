0670d7f39309a65917a6a1423febb98315c30642
kymmeh <kymmehayley@gmail.com>
2021-11-10 16:02:43 +0000

    Using mocks from test-utils

--- lib/components/repair-list/repair-list.test.tsx
+++ lib/components/repair-list/repair-list.test.tsx
@@ -1,29 +1,31 @@
 import React from "react";
-import { render, server } from "@hackney/mtfh-test-utils";
+import {
+  getRepairWorkOrdersV1,
+  mockRepairWorkOrders,
+  render,
+  server,
+} from "@hackney/mtfh-test-utils";
 import { screen, waitFor } from "@testing-library/react";
 import locale from "../../locale";
-import { getRepairsV1 } from "../../test-utils";
 import { formatDate } from "../../utils";
 import { RepairList } from "./repair-list";
 
 test("RepairList renders", async () => {
   const { container } = render(<RepairList assetId="00023400" />);
+  const mockOrder = mockRepairWorkOrders[0];
 
   await waitFor(() => {
-    screen.getByText("Electrical: Please carryout full NICEIC testing to Property (2");
+    screen.getByText(
+      `${mockOrder.tradeDescription}: ${mockOrder.description.substring(0, 50)}`,
+    );
   });
 
-  screen.getByText(formatDate("2020-09-07T09:01:09Z"));
-  screen.getByText("ELECTRICAL PLANS");
-  screen.getByText("Locked");
+  screen.getByText(formatDate(mockOrder.dateRaised));
+  screen.getByText(mockOrder.priority);
+  screen.getByText(mockOrder.status);
 
-  screen.getByText("Domestic gas: servicing: Annual Gas Service");
-  screen.getByText(formatDate("2020-08-07T20:00:55Z"));
-  screen.getByText("GAS SERVICING");
-  screen.getByText("In Progress");
-
-  expect(screen.getAllByText("Raised at:").length).toBe(2);
-  expect(screen.getAllByText("Priority:").length).toBe(2);
+  expect(screen.getAllByText("Raised at:").length).toBe(12);
+  expect(screen.getAllByText("Priority:").length).toBe(12);
 
   screen.getByText(locale.components.repairList.seeAllRepairs);
 
@@ -31,7 +33,7 @@ test("RepairList renders", async () => {
 });
 
 test("RepairList returns an error from the api", async () => {
-  server.use(getRepairsV1("error", 500));
+  server.use(getRepairWorkOrdersV1("error", 500));
 
   render(<RepairList assetId="00023400" />);
 
--- lib/test-utils.ts
+++ lib/test-utils.ts
@@ -3,11 +3,10 @@ import {
   generateMockReferenceDataV1,
   getCommentV2,
   getReferenceDataV1,
+  getRepairWorkOrdersV1,
   mockCommentReferenceDataV1,
   server,
 } from "@hackney/mtfh-test-utils";
-import { rest } from "msw";
-import { config } from "@mtfh/common/lib/config";
 
 export const mockCategoryReferenceDataV1 = Array.from({ length: 3 }).map((_, index) =>
   generateMockReferenceDataV1({
@@ -34,44 +33,10 @@ export const mockCommentsV2 = Array.from({ length: 20 }).map((_, index) =>
   }),
 );
 
-export const mockRepairs = [
-  {
-    reference: 2267941,
-    dateRaised: "2020-09-07T09:01:09Z",
-    lastUpdated: null,
-    priority: "ELECTRICAL PLANS",
-    property: "Somerford Grove Estate 103",
-    owner: "HH Electrical Planned",
-    description: "Please carryout full NICEIC testing to Property (2nd QTR test 2020/21)",
-    propertyReference: "00010773",
-    tradeCode: "EL",
-    tradeDescription: "Electrical",
-    status: "Locked",
-  },
-  {
-    reference: 2254479,
-    dateRaised: "2020-08-07T20:00:55Z",
-    lastUpdated: null,
-    priority: "GAS SERVICING",
-    property: "Somerford Grove Estate 103",
-    owner: "HH Gas Breakdown",
-    description: "Annual Gas Service",
-    propertyReference: "00010773",
-    tradeCode: "GS",
-    tradeDescription: "Domestic gas: servicing",
-    status: "In Progress",
-  },
-];
-
-export const getRepairsV1 = (data: any = mockRepairs, code = 200) =>
-  rest.get(`${config.repairsHubApiUrl}/worksOrders`, (req, res, ctx) => {
-    return res(ctx.status(code), ctx.json(data));
-  });
-
 beforeEach(() => {
   server.use(
     getCommentV2(mockCommentsV2),
     getReferenceDataV1([...mockCategoryReferenceDataV1, ...mockCommentReferenceDataV1]),
-    getRepairsV1(),
+    getRepairWorkOrdersV1(),
   );
 });
