eac1da2a41c8db966d5a7f63e68cb437b88ee0f3
Rahul Verma <rahulverma@aml0341.home>
2021-11-10 16:04:03 +0000

    feat(useWorkOrders): updated test to use msw

--- lib/api/work-order/v2/service.test.ts
+++ lib/api/work-order/v2/service.test.ts
@@ -1,15 +1,12 @@
-import { config } from "@mtfh/common/lib/config";
-import { useAxiosSWR } from "@mtfh/common/lib/http";
+import { server } from "@hackney/mtfh-test-utils";
+import { renderHook } from "@testing-library/react-hooks";
+import { rest } from "msw";
+
 import { useWorkOrders } from "./service";
 
 import { WorkOrdersFilters } from "./types";
 
-jest.mock("@mtfh/common/lib/http", () => ({
-  ...jest.requireActual("@mtfh/common/lib/http"),
-  useAxiosSWR: jest.fn(),
-}));
-
-const repairsMock = [
+const workOrdersMock = [
   {
     reference: 10036383,
     dateRaised: "2021-11-05T17:38:21.748934Z",
@@ -26,62 +23,152 @@ const repairsMock = [
   },
 ];
 
-test("useWorkOrders: it should send the correct query params to the API", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
-});
+describe("useWorkOrders", () => {
+  test("useWorkOrders: it should send the correct query params to the API", async () => {
+    const id = "00075623";
 
-test("useWorkOrders: it should send the correct query params to the API for LOCKED status", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id, WorkOrdersFilters.LOCKED);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&StatusCode=200&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
-});
+    // To be updated with mockRepairs
+    const genericWorkOrdersMock = workOrdersMock;
 
-test("useWorkOrders: it should send the correct query params to the API for IN_PROGRESS status", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id, WorkOrdersFilters.IN_PROGRESS);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&StatusCode=20%2660%2680%2690%26100%26110%26120%261000%261010%261080%261090&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
-});
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(genericWorkOrdersMock),
+        );
+      }),
+    );
+    const { result, waitForNextUpdate } = renderHook(() => useWorkOrders(id));
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
 
-test("useWorkOrders: it should send the correct query params to the API for CANCELLED status", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id, WorkOrdersFilters.CANCELLED);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&StatusCode=30&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
-});
+    expect(result.current.data).toStrictEqual(genericWorkOrdersMock);
+  });
 
-test("useWorkOrders: it should send the correct query params to the API for COMPLETED status", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id, WorkOrdersFilters.COMPLETED);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&StatusCode=40%2650&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
-});
+  test("useWorkOrders: it should send the correct query params to the API for LOCKED status", async () => {
+    const id = "00075623";
 
-test("useWorkOrders: it should send the correct query params to the API for ON_HOLD status", async () => {
-  const id = "00075623";
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(repairsMock);
-  const response = await useWorkOrders(id, WorkOrdersFilters.ON_HOLD);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.tenureApiUrlV1}/worksOrders?PageNumber=1&PageSize=12&StatusCode=10%2670&propertyReference=${id}`,
-  );
-  expect(response).toBe(repairsMock);
+    // To be updated with mockRepairs
+    const lockedWorkOrdersMock = workOrdersMock;
+
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(lockedWorkOrdersMock),
+        );
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useWorkOrders(id, WorkOrdersFilters.LOCKED),
+    );
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
+
+    expect(result.current.data).toStrictEqual(lockedWorkOrdersMock);
+  });
+
+  test("useWorkOrders: it should send the correct query params to the API for IN_PROGRESS status", async () => {
+    const id = "00075623";
+
+    // To be updated with mockRepairs
+    const inProgressWorkOrdersMock = workOrdersMock;
+
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(inProgressWorkOrdersMock),
+        );
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useWorkOrders(id, WorkOrdersFilters.IN_PROGRESS),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
+
+    expect(result.current.data).toStrictEqual(inProgressWorkOrdersMock);
+  });
+
+  test("useWorkOrders: it should send the correct query params to the API for CANCELLED status", async () => {
+    const id = "00075623";
+    // To be updated with mockRepairs
+    const cancelledWorkOrdersMock = workOrdersMock;
+
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(cancelledWorkOrdersMock),
+        );
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useWorkOrders(id, WorkOrdersFilters.CANCELLED),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
+
+    expect(result.current.data).toStrictEqual(cancelledWorkOrdersMock);
+  });
+
+  test("useWorkOrders: it should send the correct query params to the API for COMPLETED status", async () => {
+    const id = "00075623";
+    // To be updated with mockRepairs
+    const completedWorkOrdersMock = workOrdersMock;
+
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(completedWorkOrdersMock),
+        );
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useWorkOrders(id, WorkOrdersFilters.COMPLETED),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
+
+    expect(result.current.data).toStrictEqual(completedWorkOrdersMock);
+  });
+
+  test("useWorkOrders: it should send the correct query params to the API for ON_HOLD status", async () => {
+    const id = "00075623";
+    // To be updated with mockRepairs
+    const onHoldWorkOrdersMock = workOrdersMock;
+
+    server.use(
+      rest.get("/api/v1/worksOrders", (req, res, ctx) => {
+        return res(
+          ctx.status(200),
+          ctx.set("ETag", '"1"'),
+          ctx.json(onHoldWorkOrdersMock),
+        );
+      }),
+    );
+
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useWorkOrders(id, WorkOrdersFilters.ON_HOLD),
+    );
+
+    expect(result.current.data).toBe(undefined);
+    await waitForNextUpdate();
+
+    expect(result.current.data).toStrictEqual(onHoldWorkOrdersMock);
+  });
 });
