c338f941b9da1e3fecbfa2d3d6a8843f06e975a1
kymmeh <kymmehayley@gmail.com>
2021-11-22 14:53:49 +0000

    Adding add/chevron variants

--- lib/components/button/button.tsx
+++ lib/components/button/button.tsx
@@ -6,7 +6,7 @@ import { widthOverrides } from "../../utils";
 import "./styles.scss";
 
 export interface ButtonProps {
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "chevron" | "add";
   isLoading?: boolean;
   isDisabled?: boolean;
   loadingText?: string;
@@ -33,6 +33,8 @@ export const Button: ButtonComponent = forwardRef(function Button(
     "govuk-button",
     "lbh-button",
     {
+      "govuk-button--primary lbh-button--add": variant === "add",
+      "lbh-button--chevron": variant === "chevron",
       "govuk-button--secondary lbh-button--secondary": variant === "secondary",
       "lbh-button--disabled govuk-button--disabled": isDisabled,
     },
@@ -51,12 +53,48 @@ export const Button: ButtonComponent = forwardRef(function Button(
       aria-disabled={disabled}
       {...props}
     >
+      {variant === "add" && !isLoading && <AddIcon />}
       {isLoading && (
         <span className="button-loading-indicator">
           <span>Loading...</span>
         </span>
       )}
       {isLoading && loadingText ? loadingText : children}
+      {variant === "chevron" && <ChevronIcon />}
     </ButtonComp>
   );
 });
+
+const AddIcon = () => {
+  return (
+    <svg width="12" height="12" viewBox="0 0 12 12">
+      <path d="M6.94 0L5 0V12H6.94V0Z" />
+      <path d="M12 5H0V7H12V5Z" />
+    </svg>
+  );
+};
+
+const ChevronIcon = () => {
+  return (
+    <svg
+      width="20"
+      height="22"
+      viewBox="0 0 20 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 0H8.54573L20 10H11.4543L0 0Z"
+        fill="white"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 22L8.54573 22L20 10H11.4543L0 22Z"
+        fill="#96CCAE"
+      />
+    </svg>
+  );
+};
