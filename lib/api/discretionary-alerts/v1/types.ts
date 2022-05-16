0837adaa8747c30502cdb7490515ab542fed2428
dzulix <dzulix@outlook.com>
2022-05-16 16:05:19 +0200

    Update DiscretionaryAlert interface.

--- lib/api/discretionary-alerts/v1/types.ts
+++ lib/api/discretionary-alerts/v1/types.ts
@@ -1,4 +1,14 @@
 export type DiscretionaryAlert = {
   personId: string;
-  alerts: any[];
+  alerts: Alert[];
+};
+
+export type Alert = {
+  alertCode: string;
+  dateModified: string;
+  description: string;
+  endDate: string | null;
+  modifiedBy: string;
+  reason: string;
+  startDate: string;
 };
