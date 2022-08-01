d8d81f55c42604dd3e9f13448d3e9f7aab26e292
Jerome Wanliss <jerome.wanliss@hackney.gov.uk>
2022-08-01 11:32:40 +0100

    add url for cors

--- terraform/development/main.tf
+++ terraform/development/main.tf
@@ -27,7 +27,8 @@ resource "aws_s3_bucket" "frontend-bucket-development" {
       "https://manage-my-home-development.hackney.gov.uk",
       "https://temporary-accommodation-development.hackney.gov.uk",
       "https://hfs-development.hackney.gov.uk",
-      "https://manage-arrears-leasehold-development.hackney.gov.uk"
+      "https://manage-arrears-leasehold-development.hackney.gov.uk",
+      "finance-services-development.hackney.gov.uk"
     ]
     expose_headers  = ["x-amz-server-side-encryption", "x-amz-request-id", "x-amz-id-2"]
     max_age_seconds = 3000
