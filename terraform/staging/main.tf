9576d965d9277849310004bae11add99960d8482
gabimartini <gabimartinii@hotmail.com>
2022-08-02 17:32:21 +0100

    add maa staging

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -26,30 +26,31 @@ resource "aws_s3_bucket" "frontend-bucket-staging" {
     allowed_origins = [
       "https://manage-my-home-staging.hackney.gov.uk",
       "https://temporary-accommodation-staging.hackney.gov.uk",
-      "https://hfs-staging.hackney.gov.uk"
+      "https://hfs-staging.hackney.gov.uk",
+      "https://manage-arrears-leasehold-staging.hackney.gov.uk",
     ]
-    expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
+    expose_headers  = ["x-amz-server-side-encryption", "x-amz-request-id", "x-amz-id-2"]
     max_age_seconds = 3000
   }
 }
 module "cloudfront-staging" {
-  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
-  s3_domain_name = aws_s3_bucket.frontend-bucket-staging.bucket_regional_domain_name
-  origin_id = "mtfh-t-and-l-common-frontend"
-  s3_bucket_arn = aws_s3_bucket.frontend-bucket-staging.arn
-  s3_bucket_id = aws_s3_bucket.frontend-bucket-staging.id
+  source                       = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
+  s3_domain_name               = aws_s3_bucket.frontend-bucket-staging.bucket_regional_domain_name
+  origin_id                    = "mtfh-t-and-l-common-frontend"
+  s3_bucket_arn                = aws_s3_bucket.frontend-bucket-staging.arn
+  s3_bucket_id                 = aws_s3_bucket.frontend-bucket-staging.id
   orginin_access_identity_desc = "T&L common frontend cloudfront identity"
-  cname_aliases = []
-  environment_name = "staging"
-  cost_code = "B0811"
-  project_name = "MTFH Tenants and Leaseholders"
-  use_cloudfront_cert = true
-  compress = true
+  cname_aliases                = []
+  environment_name             = "staging"
+  cost_code                    = "B0811"
+  project_name                 = "MTFH Tenants and Leaseholders"
+  use_cloudfront_cert          = true
+  compress                     = true
 }
 
 resource "aws_ssm_parameter" "cdn" {
-  name  = "/housing-tl/staging/common-app-url"
-  type  = "String"
-  value = "https://${module.cloudfront-staging.cloudfront_domain_name}"
+  name      = "/housing-tl/staging/common-app-url"
+  type      = "String"
+  value     = "https://${module.cloudfront-staging.cloudfront_domain_name}"
   overwrite = true
 }
