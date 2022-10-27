debbfce86d1b0d0dac4917537e7becbb3b9a135e
biroltpx <104071063+biroltpx@users.noreply.github.com>
2022-10-27 16:02:34 +0200

    MTTL-3464 - v2 version of addProcess implemented (#204)

---
+++ lib/api/process/v2/service.ts
@@ -0,0 +1,23 @@
+import { PostProcessRequestData, Process } from "@mtfh/common/lib/api/process/v1";
+import { config } from "@mtfh/common/lib/config";
+import { axiosInstance } from "@mtfh/common/lib/http";
+
+export type PostProcessRequestDataV2 = PostProcessRequestData & {
+  patchAssignment?: {
+    patchId: string;
+    patchName: string;
+    responsibleEntityId: string;
+    responsibleName: string;
+  };
+};
+
+export const addProcess = async (
+  data: PostProcessRequestDataV2,
+  processName: string,
+): Promise<Process> => {
+  const { data: process } = await axiosInstance.post(
+    `${config.processApiUrlV2}/process/${processName}`,
+    data,
+  );
+  return process;
+};
