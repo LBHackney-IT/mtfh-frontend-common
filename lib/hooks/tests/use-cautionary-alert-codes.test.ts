bff44133b68eec677140aaa497f4e74cfceb34db
dzulix <dzulix@outlook.com>
2022-11-30 08:31:39 +0100

    Implement useCautionaryAlertCodes hook.

---
+++ lib/hooks/tests/use-cautionary-alert-codes.test.ts
@@ -0,0 +1,30 @@
+import {
+  generateMockReferenceDataV1,
+  getReferenceDataV1,
+  server,
+} from "@hackney/mtfh-test-utils";
+import { renderHook } from "@testing-library/react-hooks";
+
+import { useCautionaryAlertCodes } from "@mtfh/common/lib/hooks";
+
+describe("useCautionaryAlertCodes", () => {
+  test("it makes a call to the referenceData API for cautionary alerts and returns them correctly", async () => {
+    const code = "VA";
+    const value = "Verbal Abuse";
+    const mockRefData = [
+      generateMockReferenceDataV1({
+        category: "cautionary-alert",
+        subCategory: "alert-type",
+        code,
+        value,
+      }),
+    ];
+    server.use(getReferenceDataV1(mockRefData));
+
+    const { result, waitForNextUpdate } = renderHook(() => useCautionaryAlertCodes());
+    expect(result.current).toBe(null);
+    await waitForNextUpdate();
+
+    expect(result.current?.[code]).toStrictEqual(value);
+  });
+});
