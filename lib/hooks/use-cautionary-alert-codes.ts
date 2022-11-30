ba20a3114be7be6311425cf5fe74398dcf2bc682
Julia Wojciechowska <dzulix@outlook.com>
2022-11-30 10:54:41 +0100

    Cautionary alerts (#210)
    * Implement AddCautionaryAlert hook.
    
    * Implement useCautionaryAlertCodes hook.

---
+++ lib/hooks/use-cautionary-alert-codes.ts
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+import { useReferenceData } from "@mtfh/common/lib/api/reference-data/v1";
+
+import locale from "../locale";
+
+const { hooks } = locale;
+const { defaultCautionaryAlerts } = hooks;
+interface CautionaryAlerts {
+  [key: string]: string;
+}
+
+export const useCautionaryAlertCodes = () => {
+  const [cautionaryAlerts, setCautionaryAlerts] = useState<CautionaryAlerts>(
+    defaultCautionaryAlerts,
+  );
+
+  const { data, error } = useReferenceData<"mmh">({
+    category: "cautionary-alert",
+    subCategory: "alert-type",
+  });
+
+  useEffect(() => {
+    if (data?.mmh) {
+      const fromErr = data?.mmh.reduce((acc, obj) => {
+        acc[obj.code] = obj.value;
+        return acc;
+      }, {} as Record<string, string>);
+
+      const mergedCautionaryAlerts = { ...defaultCautionaryAlerts, ...fromErr };
+      setCautionaryAlerts(mergedCautionaryAlerts);
+    }
+  }, [data]);
+
+  if (!data && !error) {
+    return null;
+  }
+
+  return cautionaryAlerts;
+};
