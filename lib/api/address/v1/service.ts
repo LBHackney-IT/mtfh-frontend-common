cb5b27a3fba97c59a6694e3c93e04f963448908a
ange47rm <ange47rm@hotmail.com>
2023-02-28 16:01:32 +0000

    Add getAddressViaUprn call for Addresses API V1 endpoint

--- lib/api/address/v1/service.ts
+++ lib/api/address/v1/service.ts
@@ -7,14 +7,29 @@ interface AddressAPIResponse {
   data: { address: Address[] };
 }
 
-interface SearchAddressRespone {
+interface SearchAddressResponse {
   addresses?: Address[];
   error?: { code: number };
 }
 
-export const searchAddress = (postCode: string): Promise<SearchAddressRespone> =>
+export const searchAddress = (postCode: string): Promise<SearchAddressResponse> =>
   axiosInstance
     .get<AddressAPIResponse>(`${config.addressApiUrlV1}/addresses?postcode=${postCode}`, {
+      headers: {
+        "skip-x-correlation-id": true,
+      },
+    })
+    .then((res) => ({ addresses: res.data.data.address }))
+    .catch((res) => {
+      if (res.message.toLowerCase().indexOf("network") !== -1) {
+        return { error: { code: 500 } };
+      }
+      return res;
+    });
+
+export const getAddressViaUprn = (UPRN: string): Promise<SearchAddressResponse> =>
+  axiosInstance
+    .get<AddressAPIResponse>(`${config.addressApiUrlV1}/addresses?uprn=${UPRN}`, {
       headers: {
         "skip-x-correlation-id": true,
       },
