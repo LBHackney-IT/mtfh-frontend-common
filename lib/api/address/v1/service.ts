455933706eee13bbda9bca66d1c11abb293267e9
Angelo Di Massimo <70756861+ange47rm@users.noreply.github.com>
2023-02-28 22:43:58 +0000

    MR-692 - Add getAddressViaUprn call for Addresses API V1 endpoint (#228)
    * Add getAddressViaUprn call for Addresses API V1 endpoint
    
    * Added tests for service file in api\addresses\v1 to check the API is called with the correct params
    
    * Lint fixes

--- lib/api/address/v1/service.ts
+++ lib/api/address/v1/service.ts
@@ -3,18 +3,33 @@ import { AxiosSWRConfiguration, axiosInstance, useAxiosSWR } from "@mtfh/common/
 
 import type { Address } from "./types";
 
-interface AddressAPIResponse {
+export interface AddressAPIResponse {
   data: { address: Address[] };
 }
 
-interface SearchAddressRespone {
+interface SearchAddressResponse {
   addresses?: Address[];
   error?: { code: number };
 }
 
-export const searchAddress = (postCode: string): Promise<SearchAddressRespone> =>
+export const searchAddress = async (postCode: string): Promise<SearchAddressResponse> =>
   axiosInstance
     .get<AddressAPIResponse>(`${config.addressApiUrlV1}/addresses?postcode=${postCode}`, {
+      headers: {
+        "skip-x-correlation-id": true,
+      },
+    })
+    .then((res) => ({ addresses: res.data.data.address }))
+    .catch((res) => {
+      if (res.message.toLowerCase().indexOf("network") !== -1) {
+        return { error: { code: 500 } };
+      }
+      return res;
+    });
+
+export const getAddressViaUprn = async (UPRN: string): Promise<SearchAddressResponse> =>
+  axiosInstance
+    .get<AddressAPIResponse>(`${config.addressApiUrlV1}/addresses?uprn=${UPRN}`, {
       headers: {
         "skip-x-correlation-id": true,
       },
