2a36495f2ec82c4fd8ad6caf58e8534f64a9f598
Angelo Di Massimo <70756861+ange47rm@users.noreply.github.com>
2023-03-03 10:30:15 +0000

    MR-692 - Add PatchAsset API call for use in mtfh-frontend-property (#229)
    * Updated Asset interface with versionNumber property, as the BE returns this. Set this to optional to avoid potential TS issues where Asset is already in use, and set some interfaces to be exported/public
    
    * Added patchAsset in asset\v1\service to enable asset patching in mtfh-frontend-property
    
    * Added tests for patchAsset and useAsset
    
    * Lint fix

--- lib/api/asset/v1/service.ts
+++ lib/api/asset/v1/service.ts
@@ -2,14 +2,29 @@ import { config } from "@mtfh/common/lib/config";
 import {
   AxiosSWRConfiguration,
   AxiosSWRResponse,
+  axiosInstance,
   useAxiosSWR,
 } from "@mtfh/common/lib/http";
 
-import { Asset } from "./types";
+import { Asset, AssetAddress } from "./types";
 
 export const useAsset = (
   id: string | null,
   options?: AxiosSWRConfiguration<Asset>,
 ): AxiosSWRResponse<Asset> => {
   return useAxiosSWR(id && `${config.assetApiUrlV1}/assets/${id}`, options);
+};
+
+export const patchAsset = async (
+  id: string,
+  assetAddress: AssetAddress,
+  assetVersion: string,
+): Promise<void> => {
+  await axiosInstance
+    .patch(`${config.assetApiUrlV1}/assets/${id}/address`, assetAddress, {
+      headers: {
+        "If-Match": assetVersion,
+      },
+    })
+    .catch((error) => error);
 };
