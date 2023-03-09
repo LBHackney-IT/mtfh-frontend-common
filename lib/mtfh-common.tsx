9c5cedd4a7c860ac17e40e881b47771174edd9a3
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-03-09 12:23:44 +0000

    Add specific export

--- lib/mtfh-common.tsx
+++ lib/mtfh-common.tsx
@@ -1,6 +1,10 @@
+import { useAxiosSWR } from "./http";
+
 export * from "./http";
 export * from "./auth";
 export * from "./configuration";
 export * from "./components";
 export * from "./context";
 export * from "./hooks";
+
+export {useAxiosSWR, AxiosSWRError } from './http/use-axios-swr'
