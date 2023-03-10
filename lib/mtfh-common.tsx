9f72b9e3d58b7fee5f1f9f1b671ef96ddeab7327
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-03-10 12:10:28 +0000

    blah blah blha

--- lib/mtfh-common.tsx
+++ lib/mtfh-common.tsx
@@ -1,10 +1,3 @@
-import { useAxiosSWR } from "./http";
+export { axiosInstance } from './http/http'
 
-// export * from "./http";
-export * from "./auth";
-export * from "./configuration";
-export * from "./components";
-export * from "./context";
-export * from "./hooks";
-
-// export {useAxiosSWR, AxiosSWRError } from './http/use-axios-swr'
+export const Name = "Callum"
