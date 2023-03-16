f1c22d1cf90a92d571d1517763dd6a48227e6615
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-03-16 12:20:17 +0000

    Remove exports to check for circular dependency

--- lib/mtfh-common.tsx
+++ lib/mtfh-common.tsx
@@ -1,18 +1,10 @@
 export const Name = "Callum";
 
-export { TestButton } from './TestButton';
+// export { TestButton } from './TestButton';
 
-export * from "./http";
-export * from "./auth";
-export * from "./hooks/";
-// import * from "./components"
-export * from "./configuration";
-export * from "./context";
-// export * from "./hooks"
-export * from "./components";
-// export { Alert, ErrorSummary, TimeInput } from "./components"
-
-// export { Alert } from './components/alert/alert'
-// export { ErrorSummary } from './components/error-summary/error-summary'
-// export { TimeInput } from './components/time-input/time-input'
-
+// export * from "./http";
+// export * from "./auth";
+// export * from "./hooks/";
+// export * from "./configuration";
+// export * from "./context";
+// export * from "./components";
