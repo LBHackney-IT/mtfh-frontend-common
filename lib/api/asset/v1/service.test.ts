79aa6a577511cc05a86bc6aa96e25d68d60f28bf
Callum Macpherson <88662046+LBHCallumM@users.noreply.github.com>
2023-04-04 15:57:29 +0100

    [Tech Debt] Format test names (#241)
    * Correct test names for consistancy
    
    * Lintfix

--- lib/api/asset/v1/service.test.ts
+++ lib/api/asset/v1/service.test.ts
@@ -11,135 +11,141 @@ jest.mock("@mtfh/common/lib/http", () => ({
   mutate: jest.fn(),
 }));
 
-test("patchAsset: the API is called with the right parameters", async () => {
-  const assetGuid = "15adc44b-6fde-46e8-af9c-e18b1495c9ab";
-  const assetVersion = "3";
-  const assetAddress: EditAssetAddressRequest = {
-    assetAddress: {
-      uprn: "100021045676",
-      addressLine1: "FLAT B",
-      addressLine2: "51 GREENWOOD ROAD",
-      addressLine3: "HACKNEY",
-      addressLine4: "LONDON",
-      postCode: "E8 1NT",
-      postPreamble: "",
-    },
-  };
-
-  patchAsset(assetGuid, assetAddress, assetVersion);
-
-  expect(axiosInstance.patch).toBeCalledWith(
-    `${config.assetApiUrlV1}/assets/${assetGuid}/address`,
-    assetAddress,
-    { headers: { "If-Match": assetVersion } },
-  );
-});
-
-test("useAsset: the API is called with the right parameters", async () => {
-  const returnedValue: Asset = {
-    id: "15adc44b-6fde-46e8-af9c-e18b1495c9ab",
-    assetId: "100021045676",
-    assetType: "LettableNonDwelling",
-    rootAsset: "",
-    parentAssetIds: "",
-    assetLocation: {
-      floorNo: "4",
-      totalBlockFloors: 6,
-      parentAssets: [{ id: "123", name: "asset", type: "asset-type" }],
-    },
-    assetAddress: {
-      uprn: "100021045676",
-      addressLine1: "FLAT B",
-      addressLine2: "51 GREENWOOD ROAD",
-      addressLine3: "HACKNEY",
-      addressLine4: "LONDON",
-      postCode: "E8 1NT",
-      postPreamble: "",
-    },
-    assetManagement: {
-      agent: "agent",
-      areaOfficeName: "areaOfficeName",
-      isCouncilProperty: true,
-      managingOrganisation: "org",
-      managingOrganisationId: "456",
-      owner: "owner",
-      isTMOManaged: true,
-    },
-    assetCharacteristics: {
-      numberOfBedrooms: 2,
-      numberOfLifts: 1,
-      numberOfLivingRooms: 1,
-      windowType: "DBL",
-      yearConstructed: "2077",
-    },
-    tenure: null,
-    versionNumber: 2,
-    patches: [
-      {
-        id: "bd0a8e2b-c3b5-4628-aa33-8e7509d5eac6",
-        parentId: "8d4fb05d-3ff5-48b7-a17a-71fcb27a66a8",
-        name: "SN4",
-        patchType: "patch",
-        domain: "MMH",
-        responsibleEntities: [],
+describe("patchAsset", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const assetGuid = "15adc44b-6fde-46e8-af9c-e18b1495c9ab";
+    const assetVersion = "3";
+    const assetAddress: EditAssetAddressRequest = {
+      assetAddress: {
+        uprn: "100021045676",
+        addressLine1: "FLAT B",
+        addressLine2: "51 GREENWOOD ROAD",
+        addressLine3: "HACKNEY",
+        addressLine4: "LONDON",
+        postCode: "E8 1NT",
+        postPreamble: "",
       },
-    ],
-  };
-  const assetGuid = "15adc44b-6fde-46e8-af9c-e18b1495c9ab";
+    };
 
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(returnedValue);
+    patchAsset(assetGuid, assetAddress, assetVersion);
 
-  const response = await useAsset(assetGuid, undefined);
-  expect(useAxiosSWR).toBeCalledWith(
-    `${config.assetApiUrlV1}/assets/${assetGuid}`,
-    undefined,
-  );
-  expect(response).toBe(returnedValue);
+    expect(axiosInstance.patch).toBeCalledWith(
+      `${config.assetApiUrlV1}/assets/${assetGuid}/address`,
+      assetAddress,
+      { headers: { "If-Match": assetVersion } },
+    );
+  });
 });
 
-test("createAsset: the API is called with the right parameters", async () => {
-  const body: CreateAssetAddressRequest = {
-    id: "3f44819f-f3b4-4363-88b6-4575aa4bc5b0",
-    assetId: "1234",
-    parentAssetIds: "",
-    assetType: "Dwelling",
-    assetLocation: {
-      floorNo: "",
-      totalBlockFloors: 0,
-      parentAssets: [],
-    },
-    assetAddress: {
-      uprn: "100023022032",
-      addressLine1: "20000 Butfield House Stevens Avenue",
-      addressLine2: "London",
-      addressLine3: "",
-      addressLine4: "",
-      postCode: "E9 6RS",
-      postPreamble: "",
-    },
-    assetManagement: {
-      agent: "Sanctuary Housing Association",
-      areaOfficeName: "",
-      isCouncilProperty: false,
-      managingOrganisation: "London Borough of Hackney",
-      isTMOManaged: false,
-      managingOrganisationId: "c01e3146-e630-c2cd-e709-18ef57bf3724",
-      owner: "",
-    },
-    assetCharacteristics: {
-      numberOfBedrooms: 1,
-      numberOfLifts: 0,
-      numberOfLivingRooms: 0,
-      windowType: "DBL",
-      yearConstructed: "0",
-    },
-    rootAsset: "",
-    tenure: null,
-    versionNumber: 2,
-    patches: [],
-  };
+describe("useAsset", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const returnedValue: Asset = {
+      id: "15adc44b-6fde-46e8-af9c-e18b1495c9ab",
+      assetId: "100021045676",
+      assetType: "LettableNonDwelling",
+      rootAsset: "",
+      parentAssetIds: "",
+      assetLocation: {
+        floorNo: "4",
+        totalBlockFloors: 6,
+        parentAssets: [{ id: "123", name: "asset", type: "asset-type" }],
+      },
+      assetAddress: {
+        uprn: "100021045676",
+        addressLine1: "FLAT B",
+        addressLine2: "51 GREENWOOD ROAD",
+        addressLine3: "HACKNEY",
+        addressLine4: "LONDON",
+        postCode: "E8 1NT",
+        postPreamble: "",
+      },
+      assetManagement: {
+        agent: "agent",
+        areaOfficeName: "areaOfficeName",
+        isCouncilProperty: true,
+        managingOrganisation: "org",
+        managingOrganisationId: "456",
+        owner: "owner",
+        isTMOManaged: true,
+      },
+      assetCharacteristics: {
+        numberOfBedrooms: 2,
+        numberOfLifts: 1,
+        numberOfLivingRooms: 1,
+        windowType: "DBL",
+        yearConstructed: "2077",
+      },
+      tenure: null,
+      versionNumber: 2,
+      patches: [
+        {
+          id: "bd0a8e2b-c3b5-4628-aa33-8e7509d5eac6",
+          parentId: "8d4fb05d-3ff5-48b7-a17a-71fcb27a66a8",
+          name: "SN4",
+          patchType: "patch",
+          domain: "MMH",
+          responsibleEntities: [],
+        },
+      ],
+    };
+    const assetGuid = "15adc44b-6fde-46e8-af9c-e18b1495c9ab";
 
-  createAsset(body);
+    (useAxiosSWR as jest.Mock).mockResolvedValueOnce(returnedValue);
 
-  expect(axiosInstance.post).toBeCalledWith(`${config.assetApiUrlV1}/assets/`, body);
+    const response = await useAsset(assetGuid, undefined);
+    expect(useAxiosSWR).toBeCalledWith(
+      `${config.assetApiUrlV1}/assets/${assetGuid}`,
+      undefined,
+    );
+    expect(response).toBe(returnedValue);
+  });
+});
+
+describe("createAsset", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const body: CreateAssetAddressRequest = {
+      id: "3f44819f-f3b4-4363-88b6-4575aa4bc5b0",
+      assetId: "1234",
+      parentAssetIds: "",
+      assetType: "Dwelling",
+      assetLocation: {
+        floorNo: "",
+        totalBlockFloors: 0,
+        parentAssets: [],
+      },
+      assetAddress: {
+        uprn: "100023022032",
+        addressLine1: "20000 Butfield House Stevens Avenue",
+        addressLine2: "London",
+        addressLine3: "",
+        addressLine4: "",
+        postCode: "E9 6RS",
+        postPreamble: "",
+      },
+      assetManagement: {
+        agent: "Sanctuary Housing Association",
+        areaOfficeName: "",
+        isCouncilProperty: false,
+        managingOrganisation: "London Borough of Hackney",
+        isTMOManaged: false,
+        managingOrganisationId: "c01e3146-e630-c2cd-e709-18ef57bf3724",
+        owner: "",
+      },
+      assetCharacteristics: {
+        numberOfBedrooms: 1,
+        numberOfLifts: 0,
+        numberOfLivingRooms: 0,
+        windowType: "DBL",
+        yearConstructed: "0",
+      },
+      rootAsset: "",
+      tenure: null,
+      versionNumber: 2,
+      patches: [],
+    };
+
+    createAsset(body);
+
+    expect(axiosInstance.post).toBeCalledWith(`${config.assetApiUrlV1}/assets/`, body);
+  });
 });
--- lib/api/tenure/v1/service.test.ts
+++ lib/api/tenure/v1/service.test.ts
@@ -27,99 +27,109 @@ jest.mock("@mtfh/common/lib/http", () => ({
   mutate: jest.fn(),
 }));
 
-test("addPersonToTenure: it should send the right thing to the API", async () => {
-  const addPersonToTenureParams: AddPersonToTenureParams = {
-    tenureId: "id",
-    etag: "etag",
-    householdMember: {
-      id: "hhmid",
-      dateOfBirth: "",
-      fullName: "Paco el flaco",
-      isResponsible: true,
-      type: "Person",
-      personTenureType: "HouseholdMember",
-    },
-  };
-  addPersonToTenure(addPersonToTenureParams);
-  expect(axiosInstance.patch).toBeCalledWith(
-    `${config.tenureApiUrlV1}/tenures/${addPersonToTenureParams.tenureId}/person/${addPersonToTenureParams.householdMember.id}`,
-    {
-      etag: addPersonToTenureParams.etag,
-      ...addPersonToTenureParams.householdMember,
-    },
-  );
+describe("addPersonToTenure", () => {
+  test("addPersonToTenure: it should send the right thing to the API", async () => {
+    const addPersonToTenureParams: AddPersonToTenureParams = {
+      tenureId: "id",
+      etag: "etag",
+      householdMember: {
+        id: "hhmid",
+        dateOfBirth: "",
+        fullName: "Paco el flaco",
+        isResponsible: true,
+        type: "Person",
+        personTenureType: "HouseholdMember",
+      },
+    };
+    addPersonToTenure(addPersonToTenureParams);
+    expect(axiosInstance.patch).toBeCalledWith(
+      `${config.tenureApiUrlV1}/tenures/${addPersonToTenureParams.tenureId}/person/${addPersonToTenureParams.householdMember.id}`,
+      {
+        etag: addPersonToTenureParams.etag,
+        ...addPersonToTenureParams.householdMember,
+      },
+    );
+  });
 });
 
-test("useTenure: it should send the right body to the API", async () => {
-  const returnedValue = { tenureId: "" };
-  const id = "id";
-  const options: AxiosSWRConfiguration<Tenure> = { dedupingInterval: 10 };
-  (useAxiosSWR as jest.Mock).mockResolvedValueOnce(returnedValue);
+describe("useTenure", () => {
+  test("useTenure: it should send the right body to the API", async () => {
+    const returnedValue = { tenureId: "" };
+    const id = "id";
+    const options: AxiosSWRConfiguration<Tenure> = { dedupingInterval: 10 };
+    (useAxiosSWR as jest.Mock).mockResolvedValueOnce(returnedValue);
 
-  const response = await useTenure(id, options);
-  expect(useAxiosSWR).toBeCalledWith(`${config.tenureApiUrlV1}/tenures/${id}`, options);
-  expect(response).toBe(returnedValue);
+    const response = await useTenure(id, options);
+    expect(useAxiosSWR).toBeCalledWith(`${config.tenureApiUrlV1}/tenures/${id}`, options);
+    expect(response).toBe(returnedValue);
+  });
 });
 
-test("addTenure: it should send the right body to the API", async () => {
-  const params: AddTenureParams = {
-    startOfTenureDate: "",
-    tenureType: {
-      code: "",
-      description: "",
-    },
-    tenuredAsset: {
-      id: "",
-      type: "type",
-      fullAddress: "",
-      uprn: "",
-      propertyReference: "",
-    },
-  };
-  const tenureReturned = { id: "tenureId" };
-  (axiosInstance.post as jest.Mock).mockResolvedValueOnce({ data: tenureReturned });
+describe("addTenure", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const params: AddTenureParams = {
+      startOfTenureDate: "",
+      tenureType: {
+        code: "",
+        description: "",
+      },
+      tenuredAsset: {
+        id: "",
+        type: "type",
+        fullAddress: "",
+        uprn: "",
+        propertyReference: "",
+      },
+    };
+    const tenureReturned = { id: "tenureId" };
+    (axiosInstance.post as jest.Mock).mockResolvedValueOnce({ data: tenureReturned });
 
-  const response = await addTenure(params);
+    const response = await addTenure(params);
 
-  expect(axiosInstance.post).toBeCalledWith(`${config.tenureApiUrlV1}/tenures`, params);
-  expect(mutate).toBeCalledWith(
-    `${config.tenureApiUrlV1}/tenures/${tenureReturned.id}`,
-    tenureReturned,
-    false,
-  );
-  expect(response).toBe(tenureReturned);
+    expect(axiosInstance.post).toBeCalledWith(`${config.tenureApiUrlV1}/tenures`, params);
+    expect(mutate).toBeCalledWith(
+      `${config.tenureApiUrlV1}/tenures/${tenureReturned.id}`,
+      tenureReturned,
+      false,
+    );
+    expect(response).toBe(tenureReturned);
+  });
 });
 
-test("removePersonFromTenure: it should send the right body to the API", async () => {
-  const params: RemovePersonFromTenureParams = {
-    etag: "",
-    tenureId: "id",
-    householdMemberId: "hhmid",
-  };
+describe("removePersonFromTenure", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const params: RemovePersonFromTenureParams = {
+      etag: "",
+      tenureId: "id",
+      householdMemberId: "hhmid",
+    };
 
-  removePersonFromTenure(params);
+    removePersonFromTenure(params);
 
-  expect(axiosInstance.delete).toBeCalledWith(
-    `${config.tenureApiUrlV1}/tenures/${params.tenureId}/person/${params.householdMemberId}`,
-  );
+    expect(axiosInstance.delete).toBeCalledWith(
+      `${config.tenureApiUrlV1}/tenures/${params.tenureId}/person/${params.householdMemberId}`,
+    );
+  });
 });
 
-test("editTenure: it should send the right body to the API", async () => {
-  const params: EditTenureParams = {
-    id: "id",
-    etag: "",
-  };
-  const response = {
-    data: {},
-  };
-  (axiosInstance.patch as jest.Mock).mockResolvedValueOnce(response);
-  const editTenureResponse = await editTenure(params);
+describe("editTenure", () => {
+  test("it calls the api endpoint with the correct url and parameters", async () => {
+    const params: EditTenureParams = {
+      id: "id",
+      etag: "",
+    };
+    const response = {
+      data: {},
+    };
+    (axiosInstance.patch as jest.Mock).mockResolvedValueOnce(response);
+    const editTenureResponse = await editTenure(params);
 
-  expect(axiosInstance.patch).toBeCalledWith(
-    `${config.tenureApiUrlV1}/tenures/${params.id}`,
-    {
-      etag: params.etag,
-    },
-  );
-  expect(editTenureResponse).toBe(response.data);
+    expect(axiosInstance.patch).toBeCalledWith(
+      `${config.tenureApiUrlV1}/tenures/${params.id}`,
+      {
+        etag: params.etag,
+      },
+    );
+    expect(editTenureResponse).toBe(response.data);
+  });
 });
