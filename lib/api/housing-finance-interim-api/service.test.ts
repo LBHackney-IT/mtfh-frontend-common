725f7c2ee955ee15724adc0167d0752545cd1d67
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-05-24 18:25:07 +0100

    Lintfix

--- lib/api/housing-finance-interim-api/service.test.ts
+++ lib/api/housing-finance-interim-api/service.test.ts
@@ -1,12 +1,7 @@
 import { config } from "@mtfh/common/lib/config";
-import {
-  axiosInstance,
-} from "@mtfh/common/lib/http";
+import { axiosInstance } from "@mtfh/common/lib/http";
 
-import {
-  UpdateAddressDetailsRequest,
-  updateAddressDetails,
-} from "./service";
+import { UpdateAddressDetailsRequest, updateAddressDetails } from "./service";
 
 jest.mock("@mtfh/common/lib/http", () => ({
   ...jest.requireActual("@mtfh/common/lib/http"),
@@ -16,19 +11,19 @@ jest.mock("@mtfh/common/lib/http", () => ({
 describe("updateAddressDetails", () => {
   test("it calls the api", async () => {
     // Arrange
-    var propertyReference = "propertyReference"
+    const propertyReference = "propertyReference";
 
     const request: UpdateAddressDetailsRequest = {
-        postPreamble: "test",
-        addressLine1: "2 PITCAIRN HOUSE",
-        addressLine2: "ST THOMASS SQUARE",
-        addressLine3: "HACKNEY",
-        addressLine4: "LONDON",
-        postCode: "E9 6PT"
+      postPreamble: "test",
+      addressLine1: "2 PITCAIRN HOUSE",
+      addressLine2: "ST THOMASS SQUARE",
+      addressLine3: "HACKNEY",
+      addressLine4: "LONDON",
+      postCode: "E9 6PT",
     };
 
     // Act
-    await updateAddressDetails(propertyReference, request)
+    await updateAddressDetails(propertyReference, request);
 
     // Assert
     expect(axiosInstance.patch).toBeCalledWith(
