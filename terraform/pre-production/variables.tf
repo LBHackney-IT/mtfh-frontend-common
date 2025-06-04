aba02e0e8b06f986efa114c1871dcbc88c06b012
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-06-04 15:05:04 +0100

    add pre-production worflows

---
+++ terraform/pre-production/variables.tf
@@ -0,0 +1,14 @@
+variable "environment_name" {
+  type    = string
+  default = "pre-prod"
+}
+
+variable "environment_display_name" {
+  type    = string
+  default = "pre-production"
+}
+
+variable "project_name" {
+  type    = string
+  default = "Housing-Pre-Production"
+}
