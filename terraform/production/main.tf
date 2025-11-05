27f4f64af029bc2e56f0fc865e8e596b3767d745
Alex Perez-Davies <alexander.perez-davies@hackney.gov.uk>
2025-11-05 11:09:06 +0000

    start dr run

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -4,14 +4,14 @@ provider "aws" {
 }
 terraform {
   backend "s3" {
-    bucket  = "terraform-state-housing-production"
+    bucket  = "terraform-state-disaster-recovery"
     encrypt = true
     region  = "eu-west-2"
     key     = "services/t-and-l-common-frontend/state"
   }
 }
 resource "aws_s3_bucket" "frontend-bucket-production" {
-  bucket = "lbh-housing-tl-common-frontend-production.hackney.gov.uk"
+  bucket = "lbh-housing-tl-common-frontend-production-dr.hackney.gov.uk"
   acl    = "private"
   versioning {
     enabled = true
@@ -20,18 +20,18 @@ resource "aws_s3_bucket" "frontend-bucket-production" {
     index_document = "index.html"
     error_document = "error.html"
   }
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET"]
-    allowed_origins = [
-      "https://manage-my-home.hackney.gov.uk",
-      "https://hfs.hackney.gov.uk",
-      "https://finance-services.hackney.gov.uk",
-      "https://temporary-accommodation.hackney.gov.uk"
-      ]
-    expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
-    max_age_seconds = 3000
-  }
+  # cors_rule {
+  #   allowed_headers = ["*"]
+  #   allowed_methods = ["GET"]
+  #   allowed_origins = [
+  #     "https://manage-my-home.hackney.gov.uk",
+  #     "https://hfs.hackney.gov.uk",
+  #     "https://finance-services.hackney.gov.uk",
+  #     "https://temporary-accommodation.hackney.gov.uk"
+  #     ]
+  #   expose_headers  = ["x-amz-server-side-encryption","x-amz-request-id","x-amz-id-2"]
+  #   max_age_seconds = 3000
+  # }
 }
 module "cloudfront-production" {
   source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
