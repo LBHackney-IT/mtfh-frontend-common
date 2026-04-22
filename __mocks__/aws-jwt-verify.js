d9635ded10ab558af224755520ad657a2b4689f3
LBHTKarki <tuomo.karki@hackney.gov.uk>
2026-04-22 16:07:45 +0100

    try different verifier mock

--- __mocks__/aws-jwt-verify.js
+++ __mocks__/aws-jwt-verify.js
@@ -1,15 +1,22 @@
-module.exports = {
-  CognitoJwtVerifier: {
-    create: jest.fn(() => {
-      return {
-        verify: jest.fn(async (token) => {
-          if (token === "invalid-token") {
-            throw new Error("Invalid token");
-          } else {
-            return { sub: "fake-user-id", email: "test@example.com" };
-          }
-        }),
-      };
-    }),
-  },
+// module.exports = {
+//   CognitoJwtVerifier: {
+//     create: jest.fn(() => {
+//       return {
+//         verify: jest.fn(async (token) => {
+//           if (token === "invalid-token") {
+//             throw new Error("Invalid token");
+//           } else {
+//             return { sub: "fake-user-id", email: "test@example.com" };
+//           }
+//         }),
+//       };
+//     }),
+//   },
+// };
+
+export const cognitoVerifier = {
+  verify: jest.fn().mockResolvedValue({
+    sub: "test-user",
+    email: "test@example.com",
+  }),
 };
